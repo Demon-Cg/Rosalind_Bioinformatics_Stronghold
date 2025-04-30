f977dcdffe442c90e50f9ef9c1849ae894ab20b0
Demon-Cg <66010293+Demon-Cg@users.noreply.github.com>
2025-05-01 01:36:19 +0800

    Update 01.Counting_DNA_Nucleotides.py

--- 01.Counting_DNA_Nucleotides.py
+++ 01.Counting_DNA_Nucleotides.py
@@ -5,6 +5,9 @@ def counting_dna(seq):
 	G_count = seq.count('G')
 	return A_count, C_count, G_count, T_count 
 if __name__ == '__main__':
-	dna_seq = input('请输入DNA序列：').upper()
-	counts = counting_dna(dna_seq)
-	print(counts)
+	try:
+		dna_seq = input('请输入DNA序列：').upper()
+		counts = counting_dna(dna_seq)
+		print(counts)
+	except ValueError as e:
+		print(f'错误：{e}')
